23fb81a7a3c13bee93b9b77e1f11137a39b9b8c2
AgrimG <144826911+Agrim-G@users.noreply.github.com>
2026-05-20 16:50:59 +0530

    Delete test_extract.py

--- test_extract.py
+++
@@ -1,8 +0,0 @@
-from src.extractor.text_extraction import run_extraction
-
-output = run_extraction(
-    "data/raw_pdfs/syllabus.pdf",
-    "data/extracted_text/syllabus.txt"
-)
-
-print(f"Extraction completed: {output}")
